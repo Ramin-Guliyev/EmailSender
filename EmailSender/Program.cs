917e0f7f3bbe1c7a8cd37756332a2aad7851a615
Ramin-Guliyev <ra1000ishere@outlook.com>
2021-08-28 16:58:53 +0400

    Add project files.

---
+++ EmailSender/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EmailSender
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string toEmail = " which email you want to send email ";
+            string subject = "email title";
+            string content = "email content";
+            SmtpSender smtpSender = new SmtpSender();
+            await smtpSender.SendEmailAsync(toEmail,subject,content);
+
+            Console.WriteLine("Email has been sent successfully");
+
+        }
+    }
+}
---
+++ EmailSender/SmtpSender.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Mail;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace EmailSender
+{
+    public class SmtpSender
+    {
+        private string host = "smtp.gmail.com";
+        private int port = 587;
+        private bool enableSSl = true;
+        private string emailAddress = "";
+        private string password = "";
+        public Task SendEmailAsync(string toEmail, string subject, string content)
+        {
+            var client = new SmtpClient(host, port)
+            {
+                Credentials = new NetworkCredential(emailAddress, password),
+                EnableSsl = enableSSl
+            };
+            
+
+            return client.SendMailAsync(new MailMessage(emailAddress, toEmail, subject, content)
+            {
+                IsBodyHtml = true
+            });
+        }
+    }
+}
